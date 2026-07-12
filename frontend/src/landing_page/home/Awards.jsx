7da0870f91536e38860c904ef5cb96419605d58c
Siya Saxena <saxenasiya9@gmail.com>
2026-07-12 23:59:57 +0530

    commit

---
+++ frontend/src/landing_page/home/Awards.jsx
@@ -0,0 +1,48 @@
+export default function Awards() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-6 col-sm-12">
+          <img src="/media/images/largestBroker (1).svg" />
+        </div>
+
+        <div className="col-6 p-5 mt-5">
+          <h1>Largest stock broker in India</h1>
+          <p className="mb-5">
+            2+ million Zerodha clients contribute to over 15% of all retail
+            order volumes in India daily by trading and investing in:
+          </p>
+          <div className="row">
+            <div className="col-6">
+              <ul>
+                <li>
+                  <p>Futures and Options</p>
+                </li>
+                <li>
+                  <p>Commodity derivatives</p>
+                </li>
+                <li>
+                  <p>Currency derivatives</p>
+                </li>
+              </ul>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>
+                  <p>Stocks & IPOs</p>
+                </li>
+                <li>
+                  <p>Direct mutual funds</p>
+                </li>
+                <li>
+                  <p>Bonds and Govt. Securities</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <img src="media/images/pressLogos (1).png" style={{ width: "90%" }} />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/landing_page/home/Hero.jsx
@@ -0,0 +1,24 @@
+export default function Hero() {
+  return (
+    <div className="container p-5 mb-5">
+      <div className="row text-center">
+        <img
+          src="/media/images/homeHero (1).png"
+          alt="hero image"
+          className="mb-5"
+        />
+        <h1 className="mt-5">Invest in everything</h1>
+        <p>
+          Online platform to invest in stocks, derivatives, mutual funds, and
+          more
+        </p>
+        <button
+          className="p-2 btn btn-primary fs-5 mb-5"
+          style={{ width: "20%", margin: "0 auto" }}
+        >
+          Signup Now
+        </button>
+      </div>
+    </div>
+  );
+}
