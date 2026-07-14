1cfd32104942f9a4a8364b3cbeacac39f53efc53
Siya Saxena <saxenasiya9@gmail.com>
2026-07-14 23:07:14 +0530

    dashboard ui with material ui component

---
+++ dashboard/src/Components/Home.jsx
@@ -0,0 +1,11 @@
+import Dashboard from "./Dashboard";
+import TopBar from "./TopBar";
+
+export default function Home() {
+  return (
+    <>
+      <TopBar />
+      <Dashboard />
+    </>
+  );
+}
