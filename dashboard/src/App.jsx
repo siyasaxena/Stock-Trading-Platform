d1fba7f79e6c4797c788ced73a1395d10b7cc73d
Siya Saxena <saxenasiya9@gmail.com>
2026-07-26 16:29:05 +0530

    Fix case sensitivity for Home component import

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
+import Home from "./Components/Home.jsx";
 
 function App() {
   return (
